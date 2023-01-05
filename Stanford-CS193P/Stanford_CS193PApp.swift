e48843e3337df3880ec733c05502190d36858889
Merovingian <52575351+furkananter@users.noreply.github.com>
2023-01-05 20:37:50 +0300

    Initial Commit

---
+++ Stanford-CS193P/Stanford_CS193PApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_CS193PApp.swift
+//  Stanford-CS193P
+//
+//  Created by Furkan Anter on 1/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_CS193PApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
